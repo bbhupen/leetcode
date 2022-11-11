f624c641d9e6ab841df2ce356d4d4e6c04b08340
bbhupen <67063163+bbhupen@users.noreply.github.com>
2022-11-11 11:59:31 +0530

    Time: 3 ms (69.73%), Space: 6 MB (29.75%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int reverse(int x) {
+        
+        int rem, res=0;
+        
+        while(x!=0){
+            
+            rem = x%10;
+            
+            if ((res > INT_MAX/10) || (res < INT_MIN/10)){
+                return 0;
+            }
+            
+            res = res*10 + rem;
+            
+            
+            x = x/10;
+            
+        }
+        
+        return res;
+    }
+    
+
+};
+
+
+
